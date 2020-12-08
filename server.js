dcc6f4e98e2d3c1f3b556edca3c405c7e372ac83
Nightcoder <newnightcoder@gmail.com>
2020-12-08 10:55:56 +0100

    method chaining

--- server.js
+++ server.js
@@ -2,23 +2,20 @@ const http = require("http");
 
 const port = 1234;
 
-const server = http.createServer((req, res) => {
-  res.write(
-    `<body 
-        style="height:100vh; width:100vw; background-color:black; display:flex; justify-content:center; align-items:center"
-        >
-        <h1 style="color:deepskyblue; font-family:"courier">
-            Hello World!!
-        </h1>
-    </body>`
-  );
-  res.end();
-});
-
-server.listen(port, (error) => {
-  if (error) {
-    console.log("problem", error);
-  } else {
-    console.log(`server listening on port ${port}!`);
-  }
-});
+http
+  .createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      `<body style="height:100vh; width:100vw; background-color:black; display:flex; justify-content:center; align-items:center">
+  <h1 style="color:deepskyblue">Hello World!!</h1>
+  </body>`
+    );
+  })
+  .listen(port, (error) => {
+    if (error) {
+      console.log("problem", error);
+    } else {
+      console.log(`server running at port ${port}!`);
+    }
+  });
